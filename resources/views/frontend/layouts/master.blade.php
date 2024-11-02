77625dc5d07244f9680d1ee3ffb2f3efddf5fd1f
bahari <haribahari71@gmail.com>
2024-11-02 21:53:23 +0700

    add: app.blade

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -36,7 +36,7 @@ $s_agent = $loaded_agent;
         href="https://fonts.googleapis.com/css2?family=Oxanium:wght@200;300;400;500;600;700;800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
     <!-- fontawesome css link -->
-    <link rel="stylesheet" href="'public/backend/css/fontawesome-all.css'">
+    <link rel="stylesheet" href="backend/css/fontawesome-all.css">
 
     @include('partials.header-asset')
 
